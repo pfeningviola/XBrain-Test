37ff86956a336b2c493048baa8dec644234c7bda
pfeningviola <pfeningviola@gmail.com>
2019-09-13 11:12:43 -0300

    init(testproject): add application.properties and CorsConfig

---
+++ src/main/java/com/xbrain/testproject/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.xbrain.testproject;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//its necessary to accept requests from other ports or servers
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
